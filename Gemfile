45737cbad0144a0d6cdd332fb721c76cd68a3b16
Brenda Ferrufino <ferrufinob@gmail.com>
2020-10-06 16:08:48 -0400

    transferring environment

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in healthy_recipes.gemspec
+gemspec
+
+gem "rake", "~> 12.0"
---
+++ lib/healthy_recipes.rb
@@ -0,0 +1,6 @@
+require "healthy_recipes/version"
+
+module HealthyRecipes
+  class Error < StandardError; end
+  # Your code goes here...
+end
