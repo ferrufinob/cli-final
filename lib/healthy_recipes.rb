2a4b9fd584a402da1359566ce52e5b648115f51e
Brenda Ferrufino <ferrufinob@gmail.com>
2020-10-06 16:10:46 -0400

    "transferring environment"

--- lib/healthy_recipes.rb
+++ lib/healthy_recipes.rb
@@ -1,6 +1,15 @@
-require "healthy_recipes/version"
+require_relative "healthy_recipes/version"
+require_relative "./healthy_recipes/cli"
+require_relative "./healthy_recipes/scraper"
+require_relative "./healthy_recipes/course"
+require_relative "./healthy_recipes/recipe"
 
-module HealthyRecipes
-  class Error < StandardError; end
-  # Your code goes here...
-end
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+require 'colorize'
+require 'lolize'
+# module HealthyRecipes
+#   class Error < StandardError; end
+#   # Your code goes here...
+# end
---
+++ lib/healthy_recipes/cli.rb
@@ -0,0 +1,6 @@
+class HealthyRecipes::CLI
+
+    def start
+        puts "HI CLIc"
+    end
+end
