f5d2d78442a3b4faf65cd4ee443e9b10c5b40bc0
Brenda Ferrufino <ferrufinob@gmail.com>
2020-10-08 08:07:11 -0400

    "removed recursion, change to while + break"

--- lib/healthy_recipes/cli.rb
+++ lib/healthy_recipes/cli.rb
@@ -2,13 +2,14 @@
     class HealthyRecipes::CLI
     
     def start
-        @input =  nil
-        until @input == "exit"
+        input =  nil
+        until input == "exit"
             greetings
             get_courses
             list_courses
             get_user_course
-            next_step
+            puts "Type exit to exit or type any key to to view all courses again.".colorize(:magenta)
+            input = gets.strip.downcase
             system("clear")
         end
         farewell
@@ -29,13 +30,13 @@
     end 
     
     def get_courses
-        @courses = HealthyRecipes::Course.all
+        courses = HealthyRecipes::Course.all
     end
     
     def list_courses
         puts "choose a course to see recipe".colorize(:magenta)
         puts ""
-        @courses.each.with_index(1) do |course, index|
+        get_courses.each.with_index(1) do |course, index|
             puts "#{index}. #{course.name}"    
         end
     end
@@ -43,22 +44,25 @@
     def get_user_course
         # check if valid course
         chosen_course = gets.strip.to_i
-       if valid_input(chosen_course.to_i, @courses)
+        while chosen_course != nil
+       if valid_input(chosen_course.to_i, get_courses)
         show_recipes_for(chosen_course)
+        break
        else 
         puts ""
         puts "oops wrong number try again".colorize(:red)
         puts ""  
-        get_user_course
+        chosen_course = gets.strip.to_i
     end
     end
+end
     
     def valid_input(input, data)
       input.to_i <= data.length && input.to_i > 0
     end
     
     def show_recipes_for(chosen_course)
-        course = @courses[chosen_course - 1]
+        course = get_courses[chosen_course - 1]
         course.get_recipes
         
         puts ""
@@ -68,7 +72,6 @@
         puts ""
         puts "Recipes for #{course.name} loading...".colorize(:magenta)
         course.recipes.each.with_index(1) do |recipe, index|
-            sleep(2)
           puts "#{index}. #{recipe.name}"
         end  
         puts "Type the number of the recipe to see more details".colorize(:magenta)
@@ -78,18 +81,22 @@
     # shows recipe details
     def get_user_recipe(course)
         chosen_recipe = gets.strip.to_i
-        recipe = course.recipes[chosen_recipe.to_i - 1]
-        if valid_input(chosen_recipe.to_i, course.recipes)
+
+        while chosen_recipe != nil
+            recipe = course.recipes[chosen_recipe.to_i - 1]
+            if valid_input(chosen_recipe.to_i, course.recipes)
         recipe.get_ingredients
         recipe.get_instructions
         show_ingredients(recipe)
         show_instructions(recipe)
+        break
         else
             puts ""
             puts "oops wrong number, try again".colorize(:red)
-            get_user_recipe(course)        
+            chosen_recipe = gets.strip.to_i 
         end
     end
+end
     
     def show_ingredients(recipe)
         puts ""
@@ -107,13 +114,7 @@
         recipe.instructions.each.with_index(1) {|i, num| puts "#{num}. #{i}"}
         puts ""
     end
-    
-    def next_step
-        puts ""
-        puts "Type exit to exit or type any key to to view all courses again.".colorize(:magenta)
-        @input = gets.strip.downcase
-    end
-    
+     
     def farewell
         puts "       Thank you for stopping by!!!".colorize(:magenta)
         puts ""
